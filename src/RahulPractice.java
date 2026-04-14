a227ee60c318d7b5c03d05e24372d72f6626309d
sarahaldlahmah <sarah.aldlahmah@hotmail.com>
2026-04-15 01:14:10 +0300

    more locators examples

--- src/RahulPractice.java
+++ src/RahulPractice.java
@@ -6,72 +6,73 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
 public class RahulPractice {
 
-	
-	WebDriver driver = new ChromeDriver ();
-	
-	
-	@BeforeTest 
-	public void setUp () {
-		
-	driver.get("https://rahulshettyacademy.com/locatorspractice/");
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	
+	WebDriver driver = new ChromeDriver();
+
+	@BeforeTest
+	public void setUp() {
+
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
 	}
-	
-	
-	@Test (priority = 1)
-	public void Login () {
-	 //driver.findElement(By.id("inputUsername")).sendKeys("Sarah");	
-	 driver.findElement(By.cssSelector("input#inputUsername")).sendKeys("Sarah");
-	 driver.findElement(By.name("inputPassword")).sendKeys("Sarah");
-	 
-	 driver.findElement(By.cssSelector("button.submit.signInBtn")).click();
-	// driver.findElement(By.className("signInBtn")).click();
+
+	@Test(priority = 1)
+	public void Login() {
+		// driver.findElement(By.id("inputUsername")).sendKeys("Sarah");
+		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys("Rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector(".submit.signInBtn")).click();
+
+		//driver.findElement(By.cssSelector("button.submit.signInBtn")).click();
+		 driver.findElement(By.className("signInBtn")).click();
 	}
-	
-	@Test  (priority = 2)
-	public void incorrectPassandUsernameMsg () {
-		
-		
-	//	String msg= driver.findElement(By.cssSelector("p[class = 'error']")).getText();
-	//	System.out.println(msg);
-		
-		
+
+	@Test(priority = 2, enabled = false)
+	public void incorrectPassandUsernameMsg() {
+
+		// String msg= driver.findElement(By.cssSelector("p[class =
+		// 'error']")).getText();
+		// System.out.println(msg);
+
 		String msg = driver.findElement(By.cssSelector(".error")).getText();
 		System.out.println(msg);
-				
+
 	}
-	
-	@Test (priority = 3, enabled = true) 
-	public void forgotPasswordLink () {
+
+	@Test(priority = 3, enabled = false)
+	public void forgotPasswordLink() {
 		driver.findElement(By.linkText("Forgot your password?")).click();
-		
+
 	}
-	
-	@Test (priority = 4,enabled = true )
+
+	@Test (priority = 4,enabled = false )
 	
 	public void ForgotPasspage () {
 		
 		//driver.findElement(By.xpath("//input [@placeholder = 'Name']")).sendKeys("Sarah");
-	     driver.findElement(By.xpath("//input [@placeholder = 'Name']")).sendKeys("Test");
-		driver.findElement(By.xpath("//input [@placeholder = 'Email']")).sendKeys("Sarah@gmail.com");
-		//driver.findElement(By.xpath("//input [@type= 'text'] [3]")).sendKeys("000");
-      //  driver.findElement(By.className("go-to-login-btn")).click();
-		driver.findElement(By.xpath("//input[@placeholder ='Phone Number']")).sendKeys("088155887");
-		driver.findElement(By.cssSelector("input[placeholder ='Phone Number']")).clear();
+	    driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Rahul");
+		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("4558588");
+//		driver.findElement(By.xpath("//input [@type= 'text'] [3]")).sendKeys("000");
+//        driver.findElement(By.className("go-to-login-btn")).click();
+//		driver.findElement(By.xpath("//input[@placeholder ='Phone Number']")).sendKeys("088155887");
+//		driver.findElement(By.cssSelector("input[placeholder ='Phone Number']")).clear();
+
+	//driver.findElement(By.className("reset-pwd-btn")).click();
+	//driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+	//driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+	//driver.findElement(By.cssSelector("button[class = 'reset-pwd-btn']")).click();
+	//driver.findElement(By.xpath("//button[@class='reset-pwd-btn']")).click();
+	driver.findElement(By.xpath("//div/button[2]")).click();
+	String infoMsg = driver.findElement(By.className("infoMsg")).getText();
+	System.out.println(infoMsg);
+		
+	
 		
 		
-		}
-	
-	
-	
-	
-	
-	
-
-
+		
+	}
 
 }
